a47b623fdaf9aaf27c4f845afd6102bf616470f8
Siversea <destampes.it@yahoo.com>
2026-01-08 14:30:02 +0100

    Delete scripts directory

--- scripts/genreMusical.sh
+++
@@ -1,6 +0,0 @@
-read -p "quel genre de musique? " genre
-mkdir genre/$genre
-touch genre/$genre/information.txt
-echo $genre > genre/$genre/information.txt
-read -p "description du genre de musique. " description
-echo $description >> genre/$genre/information.txt
