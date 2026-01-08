87f4209bce65d7af98965dd594f161db44045ebf
Nico59710 <nicolas.pollet@live.fr>
2026-01-08 11:57:12 +0100

    Script 1 v1.2

--- scripts/genreMusical.sh
+++ scripts/genreMusical.sh
@@ -1,6 +1,6 @@
 read -p "quel genre de musique? " genre
-mkdir $genre
-touch $genre/information.txt
-echo $genre > $genre/information.txt
+mkdir genre/$genre
+touch genre/$genre/information.txt
+echo $genre > genre/$genre/information.txt
 read -p "description du genre de musique. " description
-echo $description >> $genre/information.txt
+echo $description >> genre/$genre/information.txt
